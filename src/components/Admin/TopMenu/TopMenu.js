fb3060a80dba21abc23e39d71bb274e405a6bb0f
Fernando <fernando@pop-os.localdomain>
2023-06-22 19:08:33 -0300

    Restricciones de usuarios

--- src/components/Admin/TopMenu/TopMenu.js
+++ src/components/Admin/TopMenu/TopMenu.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import { Icon, Menu } from 'semantic-ui-react'
+import { useAuth } from '../../../hooks'
 import './TopMenu.scss'
 
 export function TopMenu () {
+  const { auth, logout } = useAuth()
+
+  const renderName = () => {
+    if (auth.me?.first_name && auth.me?.last_name) {
+      return `${auth.me.first_name} ${auth.me.last_name}`
+    }
+    return auth.me?.email
+  }
+
   return (
     <Menu fixed='top' className='top-menu-admin'>
       <Menu.Item className='top-menu-admin__logo'>
@@ -10,8 +20,8 @@ export function TopMenu () {
       </Menu.Item>
 
       <Menu.Menu position='right'>
-        <Menu.Item>Hola, usuario...</Menu.Item>
-        <Menu.Item>
+        <Menu.Item>Hola, {renderName()}</Menu.Item>
+        <Menu.Item onClick={logout}>
           <Icon name='sign-out' />
         </Menu.Item>
       </Menu.Menu>
