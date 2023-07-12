bb88b8c5639a3a555b1ef965a987358c3b83062b
Fernando <fernando@pop-os.localdomain>
2023-07-12 19:34:17 -0300

    Mejoras en formulario de creación de usuarios

--- src/components/Admin/Users/AddEditUserForm/AddEditUserForm.js
+++ src/components/Admin/Users/AddEditUserForm/AddEditUserForm.js
@@ -1,25 +1,103 @@
 import React from 'react'
 import { Form, Button, Checkbox } from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import './AddEditUserForm.scss'
 
 export function AddEditUserForm () {
+  const formik = useFormik({
+    initialValues: initalValues(),
+    validationSchema: Yup.object(newValidationSchema()),
+    validateOnChange: false,
+    onSubmit: formValue => {
+      console.log('Formulario enviado')
+      console.log(formValue)
+    }
+  })
+
   return (
-    <Form className='add-edit-user-form'>
-      <Form.Input name='username' placeholder='Nombre de usuario' />
-      <Form.Input name='email' placeholder='Correo electrónico' />
-      <Form.Input name='first_name' placeholder='Nombre' />
-      <Form.Input name='last_name' placeholder='Apellido' />
-      <Form.Input name='password' placeholder='Contraseña' />
+    <Form className='add-edit-user-form' onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name='username'
+        placeholder='Nombre de usuario'
+        value={formik.values.username}
+        onChange={formik.handleChange}
+        error={formik.errors.username}
+      />
+      <Form.Input
+        name='email'
+        placeholder='Correo electrónico'
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name='first_name'
+        placeholder='Nombre'
+        value={formik.values.first_name}
+        onChange={formik.handleChange}
+        error={formik.errors.first_name}
+      />
+      <Form.Input
+        name='last_name'
+        placeholder='Apellido'
+        value={formik.values.last_name}
+        onChange={formik.handleChange}
+        error={formik.errors.last_name}
+      />
+      <Form.Input
+        name='password'
+        placeholder='Contraseña'
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
 
       <div className='add-edit-user-form__active'>
-        <Checkbox toggle /> Usuario activo
+        <Checkbox
+          toggle
+          checked={formik.values.is_active}
+          onChange={(_, data) =>
+            formik.setFieldValue('is_active', data.checked)
+          }
+        />
+        Usuario activo
       </div>
 
       <div className='add-edit-user-form__staff'>
-        <Checkbox toggle /> Usuario administrador
+        <Checkbox
+          toggle
+          checked={formik.values.is_staff}
+          onChange={(_, data) => formik.setFieldValue('is_staff', data.checked)}
+        />
+        Usuario administrador
       </div>
 
       <Button type='submit' primary fluid content='Crear' />
     </Form>
   )
+}
+
+function initalValues () {
+  return {
+    username: '',
+    email: '',
+    first_name: '',
+    last_name: '',
+    is_active: true,
+    is_staff: false,
+    password: ''
+  }
+}
+
+function newValidationSchema () {
+  return {
+    username: Yup.string().required(true),
+    email: Yup.string().email(true).required(true),
+    first_name: Yup.string(),
+    last_name: Yup.string(),
+    is_active: Yup.bool().required(true),
+    is_staff: Yup.bool().required(true),
+    password: Yup.string().required(true)
+  }
 }
